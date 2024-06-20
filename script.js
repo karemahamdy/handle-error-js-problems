1bae4478a7194a70bb176da0c039e2a9127c53eb
Karema <karemahamdy51@gmail.com>
2024-06-20 23:06:28 +0300

    finish

--- script.js
+++ script.js
@@ -78,3 +78,74 @@ function checkInvalidArray(arr , index) {
 }
 }
 checkInvalidArray([1, 2, 3, 4, 5] , 1)
+
+// Write a JavaScript program that demonstrates the use of the 'try-catch-finally' statement to catch and handle an error, and then execute some cleanup code in the 'finally' block.
+// Define a function named divide_Numbers that takes two parameters: x and y
+function divide_Numbers(x, y) {
+  try {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new TypeError('Invalid arguments. Both arguments should be numbers.');
+    }
+    if (y === 0) {
+
+      throw new Error('Invalid divisor. Cannot divide by zero.');
+    }
+    
+    const result = x / y;
+  
+    console.log('Result:', result);
+  } catch (error) {
+    
+    console.log('Error:', error.message);
+  } finally {
+      
+    console.log('Cleanup code executed.');
+  }
+}
+
+divide_Numbers(10, 2);  
+divide_Numbers(10, 0); 
+divide_Numbers(10, '2'); 
+
+
+//Write a JavaScript program that uses a try-catch block to catch and handle a 'ReferenceError' when accessing an undefined variable.
+
+// Define a function named access_Variable that attempts to access an undefined variable
+function access_Variable() {
+  try {
+    console.log(undefinedVariable);
+  } catch (error) {  
+    if (error instanceof ReferenceError) {
+      console.log('ReferenceError:', error.message);
+    } else {
+      console.log('Error:', error.message);
+    }
+  }
+}
+access_Variable();
+
+
+// Write a JavaScript program that uses a try-catch block to catch and handle a 'TypeError' when accessing a property of an undefined object.
+// Try block to handle potential errors
+try {
+  // Declare a constant variable undefinedObject and assign it the value undefined
+  const undefinedObject = undefined;
+  // Access a property of the undefined object, which will result in an error
+  console.log(undefinedObject.property); // Accessing property of undefined object
+} 
+// Catch block to handle errors thrown in the try block
+catch (error) {
+  // Check if the error is an instance of TypeError
+  if (error instanceof TypeError) 
+  {
+    // Log an error message indicating property access to an undefined object
+    console.log('Error: Property access to undefined object');
+  } 
+  // If the error is not a TypeError, rethrow the error
+  else 
+  {
+    throw error; // Rethrow the error if it's not a TypeError
+  }
+}
+
+
